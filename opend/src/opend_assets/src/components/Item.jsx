1b6200d38be177c11289a921cd9e9b0ffd510f0a
Hookypoo <forde.hugh@yahoo.com>
2022-08-18 14:53:32 -0400

    add nft owner title and image

--- opend/src/opend_assets/src/components/Item.jsx
+++ opend/src/opend_assets/src/components/Item.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
-import { HttpAgent } from "@dfinity/agent"
+import { Actor, HttpAgent } from "@dfinity/agent";
 import { idlFactory } from "../../../declarations/nft";
 import { Principal } from "@dfinity/principal";
 
 function Item(props) {
-
   const [name, setName] = useState();
+  const [owner, setOwner] = useState();
+  const [image, setImage] = useState();
 
-    const id = Principal.fromText(props.id);
+    const id = Principal.fromText(props.id);     
 
-    const localHost = "https://localhost:8080";
+    const localHost = "http://172.30.136.180:8080/";
     const agent = new HttpAgent({host: localHost});
 
     async function loadNFT() {
@@ -19,27 +20,35 @@ function Item(props) {
       canisterId: id,
     });
 
-    const name = await NFTActor.getName();
-    setName(name);
+    const name = await NFTActor.getName();        
+    const owner = await NFTActor.getOwner(); 
+    const imageData = await NFTActor.getAsset(); 
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(new Blob([imageContent.buffer], {type: "image/png" })
+    );
+
+    setName(name); 
+    setOwner(owner.toText());
+    setImage(image);
   }
 
   useEffect(() => {
     loadNFT();
-  }, [])
+  }, []);
 
   return (
     <div className="disGrid-item">
       <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
         <img
           className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
+          src={ image }
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            {name}<span className="purple-text"></span>
+            { name }<span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
+            Owner: { owner }
           </p>
         </div>
       </div>
